bfd503d3fec53773cee1733cb06c005c46898430
TayLQ <luongquoctay87@gmail.com>
2022-03-18 08:20:38 +0700

    sample project base for trainee

---
+++ src/test/java/com/trainingfresher/sampleservice/SampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingfresher.sampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
