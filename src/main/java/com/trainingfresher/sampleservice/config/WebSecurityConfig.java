4bb072458dcd7b7a726837ceb0bfb56146401876
haiduy123 <bilatao02@gmail.com>
2022-03-22 22:14:50 +0700

    swagger UI

--- src/main/java/com/trainingfresher/sampleservice/config/WebSecurityConfig.java
+++ src/main/java/com/trainingfresher/sampleservice/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package com.trainingfresher.sampleservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,10 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v2/api-docs/**");
+    }
 }
